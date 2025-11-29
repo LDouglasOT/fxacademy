1e8bb1ba8605b69d929c28734cab804776863543
LDouglasOT <douglas.luzinda@gmail.com>
2025-11-29 06:52:13 +0300

    ed

--- fxAcademy/settings.py
+++ fxAcademy/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fxAcademy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'fxacademy_db'),
+        'USER': os.environ.get('DB_USER', 'fxacademy'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'fxacademy'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
