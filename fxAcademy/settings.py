3933e7a097c0a7ee7b9f695b301115d489ccd9d3
Ubuntu <ubuntu@ip-172-31-17-170.eu-north-1.compute.internal>
2025-12-05 06:14:58 +0000

    wazzination

--- fxAcademy/settings.py
+++ fxAcademy/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-i%9ybye+wkisvc2a_3svfj_%a190i3+i*8f9cj91w(9gdd7b=a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
